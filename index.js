5368c27745a64dd807cabc7747c2087fb94f0083
DKendle <dina.kendle@gmail.com>
2021-06-05 09:39:19 -0400

    commit tree 8eac2c1b49a6a1bbd9b1c2954ff5cc2fe87e6df6

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+    })
+
+}
+
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(json){
+        console.log(JSON.parse(json))
+    })
+}
